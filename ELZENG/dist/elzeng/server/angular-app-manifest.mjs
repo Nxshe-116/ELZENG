020c784c919e4eaf6cada6624edb5daf95f709b9
Nashe Chagumaira <77992898+Nxshe-116@users.noreply.github.com>
2025-03-25 15:54:54 +1100

    deployed

--- ELZENG/dist/elzeng/server/angular-app-manifest.mjs
+++ ELZENG/dist/elzeng/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 24330, hash: '824d4164073c8ac7e253f80beabab59150f4c04a610928d2d87f9325c430ec9a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 19867, hash: 'f8f77404d17fc202a8986bf839950921dd685e06080a5d2461b4d138f0635fcb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-447FLUYU.css': {size: 29661, hash: '8q2A9A+d0AE', text: () => import('./assets-chunks/styles-447FLUYU_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 24330, hash: '975658690699523873ca4ae0a57945ea3cd867c0090c6ac0218b6a03678cfe58', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 19867, hash: 'f6d552f0a0a6485105e38cef06ffbb3bf0d27fe3f68e75a527a82ef4cf7914f1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-FAOUBUOY.css': {size: 30199, hash: 'MbfxH9P1IQg', text: () => import('./assets-chunks/styles-FAOUBUOY_css.mjs').then(m => m.default)}
   },
 };
